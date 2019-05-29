216571caacdd882063677ff21cefd77385143735
Andy Gray <andy@whatsthescore.co.uk>
2019-05-29 17:08:32 +0100

    tweaking

--- src/main.js
+++ src/main.js
@@ -5,13 +5,16 @@ import store from './store';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Vue2Filters from 'vue2-filters';
 
 Vue.use(BootstrapVue);
+Vue.use(Vue2Filters);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  mixins: [Vue2Filters.mixin],
   render: h => h(App)
 }).$mount('#app');
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,7 +2,7 @@
   <div>
     <page-title text="Game" class="mt-3"></page-title>
 
-
+    <div v-if="loading">Loading...</div>
 
     <div class="row" v-if="!loading && !(home && away)">
       <div class="col">
@@ -16,41 +16,57 @@
         </b-form-group>
       </div>
     </div>
+    <div v-else-if="!homeTopTeam && !awayTopTeam && home && away" class="text-center">
+      <b-button variant="outline-primary" @click="start">Start</b-button>
+    </div>
 
-
-    <div v-else-if="homeTopTeam && awayTopTeam" class="text-center">
+    <div v-if="home && away" class="text-center">
       <div class="row mt-5">
         <div class="col">
-          <h1>{{ home.substr(0, 6) }} [{{ homeTopTeam.topTeam.teamAverageFloored }}]</h1>
+          <h1>{{ home.substr(0, 6) }} <span v-if="homeTopTeam">[{{ homeTopTeam.topTeam.teamAverageFloored }}]</span></h1>
         </div>
         <div class="col">
           <h1>VS</h1>
         </div>
         <div class="col">
-          <h1>{{away.substr(0, 6) }} [{{ awayTopTeam.topTeam.teamAverageFloored }}]</h1>
+          <h1>{{away.substr(0, 6) }} <span v-if="awayTopTeam">[{{ awayTopTeam.topTeam.teamAverageFloored }}]</span></h1>
         </div>
       </div>
 
       <div class="row mt-5" v-if="homeStats && awayStats">
         <div class="col"><h1>{{ homeStats.goals }}</h1></div>
         <div class="col">
-          <h1>{{ currentMin }}</h1>
+          <h1 v-if="!complete">{{ currentMin }}</h1>
+          <h1 v-else>RESULT</h1>
         </div>
         <div class="col"><h1>{{ awayStats.goals }}</h1></div>
       </div>
-
 
       <div class="row mt-5" v-if="homeStats && awayStats">
         <div class="col">
           <div v-for="evt in homeStats.majorEvents" class="text-left ml-5">
             <span v-if="evt.eventType === 'goal'">⚽</span>
             <span v-if="evt.eventType === 'yellow'">🧽</span>
-            
+
             {{ evt.time }}. {{ evt.fullName }}
           </div>
         </div>
         <div class="col">
-
+          <div class="row">
+            <div class="col">{{ filterBy(homeStats.minorEvents, 'shot', 'eventType').length }}</div>
+            <div class="col">Shots</div>
+            <div class="col">{{ filterBy(awayStats.minorEvents, 'shot', 'eventType').length }}</div>
+          </div>
+          <div class="row">
+            <div class="col">{{ filterBy(homeStats.minorEvents, 'corner', 'eventType').length }}</div>
+            <div class="col">Corners</div>
+            <div class="col">{{ filterBy(awayStats.minorEvents, 'corner', 'eventType').length }}</div>
+          </div>
+          <div class="row">
+            <div class="col">{{ filterBy(homeStats.majorEvents, 'yellow', 'eventType').length }}</div>
+            <div class="col">Yellows</div>
+            <div class="col">{{ filterBy(awayStats.majorEvents, 'yellow', 'eventType').length }}</div>
+          </div>
         </div>
         <div class="col">
           <div v-for="evt in awayStats.majorEvents" class="text-left ml-5">
@@ -63,8 +79,6 @@
       </div>
 
     </div>
-
-    <b-button variant="outline-primary" @click="start" v-else>Start</b-button>
   </div>
 </template>
 
@@ -74,16 +88,18 @@
   import { dotDotDotAccount, waitForMillis } from '../utils';
   import _ from 'lodash';
   import NiftyFootballEngine from '../utils/NiftyFootballEngine';
+  import Vue2Filters from 'vue2-filters';
 
   export default {
     name: 'game',
+    mixins: [Vue2Filters.mixin],
     components: {PageTitle},
-    mixins: [],
     data() {
       return {
         topTeams: [],
         topTeamsOptions: [],
         loading: false,
+        complete: false,
         home: null,
         homeTopTeam: null,
         away: null,
@@ -98,6 +114,7 @@
       ...mapState([
         'cardsApiService',
       ]),
+
     },
     methods: {
       loadTopTeams() {
@@ -114,6 +131,7 @@
           });
       },
       async start() {
+        this.loading = true;
         this.homeTopTeam = await this.cardsApiService.loadTopTeam(this.home);
         this.awayTopTeam = await this.cardsApiService.loadTopTeam(this.away);
 
@@ -123,6 +141,8 @@
         const TOTAL_ITERATIONS = 90 + _.random(2, 7);
 
         this.engine.init(this.homeTopTeam.topTeam, this.awayTopTeam.topTeam);
+
+        this.loading = false;
 
         for (; this.currentMin < TOTAL_ITERATIONS; this.currentMin++) {
           const homeAttackFormation = this.engine.attackFormation(HOME);
@@ -140,6 +160,8 @@
 
           await waitForMillis(200);
         }
+
+        this.complete = true;
       }
     },
     async created() {
